92d1ece1c12fd66d87d742cdc08c75b95ba793eb
Lukas <seriouz.accz@gmail.com>
2017-03-29 20:58:33 +0200

    Pull api root to env var

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-
+export REACT_APP_API_ROOT=https://lunchagator.herokuapp.com
+npm run build
 firebase deploy --only hosting
--- src/api/dailyMenuService.js
+++ src/api/dailyMenuService.js
@@ -1,6 +1,6 @@
-const apiRoot = 'http://localhost:8080/menu'
+const apiRoot = process.env.REACT_APP_API_ROOT
 
 export function getMenus() {
-    return fetch(`${apiRoot}/today`)
+    return fetch(`${apiRoot}/menu/today`)
         .then(r => r.json())
 }
